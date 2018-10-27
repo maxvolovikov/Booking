72c176ea090970743337df34e805275f3acb151e
Joshua Marx <marx.joshua@gmail.com>
2018-10-26 23:03:45 -0700

    setting up controller module for db queries

--- server/index.js
+++ server/index.js
@@ -8,10 +8,14 @@ app.use(express.static(path.join(__dirname, '..', '/public')));
 
 app.get('/rooms/:listingId', (req, res) => {
   // TODO: Setup GET req to db
+  // Calls the controller module which issues a getter query
+  // to the database asking for a particular listing with id
 });
 
 app.post('/bookings/:bookingId', (req, res) => {
   // TODO: Setup POST req to DB
+  // Calls the controller module which issues a setter query
+  // to the database asking to create a new booking with id
 });
 
 app.listen(port, () => {
