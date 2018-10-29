74d6888ca4268d1ad261bc22b25700510ce83668
Joshua Marx <marx.joshua@gmail.com>
2018-10-29 10:54:41 -0700

    built out GET API for user and customer names

--- server/index.js
+++ server/index.js
@@ -8,22 +8,49 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, '..', '/public')));
 app.use(express.json());
 
-app.get('/rooms/:listingId', (req, res) => {
-  controller.get(req.params.listingId)
+app.get('/users/:userId', (req, res) => {
+  controller.user.get(req.params.userId)
     .then((data) => {
-      if (data) res.send(data).end();
-      res.send('No listing found').end();
+      if (data) {
+        res.send(data.name).end();
+      } else {
+        res.send('User not found').end();
+      }
+    })
+    .catch((err) => { throw err; });
+});
+
+app.get('/customers/:customerId', (req, res) => {
+  controller.customer.get(req.params.customerId)
+    .then((data) => {
+      if (data) {
+        res.send(data.name).end();
+      } else {
+        res.send('Customer not found').end();
+      }
+    })
+    .catch((err) => { throw err; });
+});
+
+app.get('/rooms/:listingId', (req, res) => {
+  controller.listing.get(req.params.listingId)
+    .then((data) => {
+      if (data) {
+        res.send(data).end();
+      } else {
+        res.status(404).send('No listing found').end();
+      }
     })
     .catch((err) => {
       console.error('Error: GET Req', err);
-      res.send(`Response Error getting listing ${req.params.listingId}`).end();
+      res.status(500).send(`Response Error getting listing ${req.params.listingId}`).end();
     });
 });
 
 app.post('/bookings/:bookingId', (req, res) => {
-  controller.set(req.query, req.params.bookingId)
+  controller.booking.set(req.query, req.params.bookingId)
     .then(() => { res.send('Booking successful. Enjoy!').end(); })
-    .catch(() => { res.send('Could not create booking. Please try again').end(); });
+    .catch(() => { res.status(500).send('Could not create booking. Please try again').end(); });
 });
 
 app.listen(port, () => {
