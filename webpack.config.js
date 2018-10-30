d672d1a080b9809dfc4080cf11cb559021a51591
Joshua Marx <marx.joshua@gmail.com>
2018-10-30 14:22:27 -0700

    included css and styler loaders for .css files

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: [/\.jsx?$/],
         resolve: {
           extensions: ['.js', '.jsx'],
