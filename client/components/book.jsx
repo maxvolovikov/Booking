0c1f453d067d0eb117979cf3429e348e647614d9
Joshua Marx <marx.joshua@gmail.com>
2018-10-29 22:42:47 -0700

    added button className

--- client/components/book.jsx
+++ client/components/book.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Book = ({ handleOnSubmit }) => (
   <div>
     <h3>Book Button</h3>
-    <button type="submit" onClick={handleOnSubmit}>Book</button>
+    <button className="book-button" type="submit" onClick={handleOnSubmit}>Book</button>
   </div>
 );
 
