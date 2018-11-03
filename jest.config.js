b72abf7ddb299a67a47475bd44e4aa77a07b85ca
Joshua Marx <marx.joshua@gmail.com>
2018-11-03 12:34:56 -0700

    updated jest config to fix .css import error

--- jest.config.js
+++ jest.config.js
@@ -74,7 +74,10 @@ module.exports = {
 
   // A map from regular expressions to module names that allow to stub out
   // resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered
   // 'visible' to the module loader
