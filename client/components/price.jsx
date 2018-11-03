12f5bf90c23d73428d04a4b0080512aec785d688
Joshua Marx <marx.joshua@gmail.com>
2018-11-03 12:10:48 -0700

    refactored for linting/styling. Added logic to display price breakdown after total is computed

--- client/components/price.jsx
+++ client/components/price.jsx
@@ -8,20 +8,71 @@ const Price = ({
   service_fee,
   day_rate,
   total,
-}) => (
-  <div className="price">
-    <div className="rate">${day_rate} per night</div>
-    <div className="fee">Cleaning: ${cleaning_fee}</div>
-    <div className="fee">Service: ${service_fee}</div>
-    <div className="total">Total: ${total}</div>
-  </div>
-);
+  daysBooked,
+}) => {
+  const confirmBooking = (
+    <div className="confirm-booking">
+      <div className="nights">
+        $
+        {day_rate}
+        {' '}
+        x
+        {' '}
+        {daysBooked}
+        {' '}
+        nights
+        {' '}
+        <span className="cost">
+          {' '}
+          $
+          {day_rate * daysBooked}
+        </span>
+      </div>
+      <div className="fee">
+        Cleaning fee
+        <span className="cost">
+          {' '}
+          $
+          {cleaning_fee}
+        </span>
+      </div>
+      <div className="fee">
+      Service fee
+        <span className="cost">
+          {' '}
+          $
+          {service_fee}
+        </span>
+      </div>
+      <div className="total">
+      Total
+        <span className="cost">
+          {' '}
+          $
+          {total}
+        </span>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="price">
+      <div className="rate">
+        $
+        {day_rate}
+        <span className="per-night"> per night</span>
+      </div>
+      {total > 0 ? confirmBooking : null}
+    </div>
+  );
+};
 
 Price.propTypes = {
   day_rate: PropTypes.number,
   cleaning_fee: PropTypes.number,
   service_fee: PropTypes.number,
   total: PropTypes.number,
+  daysBooked: PropTypes.number,
 };
 
 Price.defaultProps = {
@@ -29,6 +80,7 @@ Price.defaultProps = {
   day_rate: 0,
   cleaning_fee: 0,
   service_fee: 0,
+  daysBooked: 0,
 };
 
 export default Price;
