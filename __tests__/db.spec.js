05ae0a38fc8d8c11dfcd2857b6b7c7dcf766dc95
Joshua Marx <marx.joshua@gmail.com>
2018-11-03 18:04:18 -0700

    added set and delete mock data to db

--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -30,14 +30,22 @@ describe('Database Queries', () => {
   });
 
   it('sets a booking to the database', () => {
-    // TODO: POST a fake booking to the database
-    // look for it, return it, and delete it
-    const booking = controller.booking.get(1);
-    return booking.then((data) => {
-      expect(data.id).to.equal(1);
-      expect(data.listing_id).to.exist;
-      expect(data.start_date).to.exist;
-      expect(data.host_booking).to.be.a('boolean');
-    });
+    const mockBooking = {
+      listing_id: 1001,
+      customer_id: 1000,
+      start_date: 1000,
+      end_date: 1000,
+      total_cost: 1000,
+      host_booking: false,
+    };
+
+    return controller.booking.set(mockBooking)
+      .then(() => controller.booking.get('listing_id', 1001))
+      .then((data) => {
+        expect(data.listing_id).to.exist;
+        expect(data.start_date).to.exist;
+        expect(data.host_booking).to.be.a('boolean');
+      })
+      .then(() => { controller.booking.delete('listing_id', 1001); });
   });
 });
--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -3,6 +3,7 @@
 import { expect } from 'chai';
 import fetch from 'supertest';
 import api from '../server/api';
+import db from '../db/controllers/index';
 
 describe('Server API', () => {
   it('should respond with 200 to a GET at /',
@@ -29,12 +30,25 @@ describe('Server API', () => {
         expect(res.statusCode).to.equal(404);
       }));
 
-  it('should respond with 200 to POST at /booking',
-    // TODO: Shoudl post a new entry to the database
-    // and then remove on success
+  it('should respond with 200 to POST at /booking', () => {
+    const mockBooking = {
+      listing_id: 1000,
+      customer_id: 1000,
+      start_date: 1000,
+      end_date: 1000,
+      total_cost: 1000,
+      host_booking: false,
+    };
 
-    () => fetch(api).post('/booking')
+    return fetch(api).post('/booking')
+      .send(mockBooking)
       .then((res) => {
         expect(res.statusCode).to.equal(200);
-      }));
+      })
+      .then(() => db.booking.get('listing_id', 1000))
+      .then((data) => {
+        expect(data.listing_id).to.equal(1000);
+      })
+      .then(() => { db.booking.delete('listing_id', 1000); });
+  });
 });
